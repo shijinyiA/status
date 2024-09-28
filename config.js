e0f42eca311ee84a3cfd27d803ca9e9cff3c085b
锦衣 <hengxing1774006@163.com>
2024-09-28 12:53:21 +0800

    1

---
+++ config.js
@@ -0,0 +1,17 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '锦衣-监控页面',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2689816-d415de83e3b499e068eed891',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
